098d30cf3cd93365a606a6d1166a42619d850365
NicolasJbo <57155671+NicolasJbo@users.noreply.github.com>
2021-10-24 23:12:13 -0300

    Vistas

--- OperativaII-Angular/src/app/app-routing.module.ts
+++ OperativaII-Angular/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '**', component : AboutUsComponent }
 ];
 
 @NgModule({
