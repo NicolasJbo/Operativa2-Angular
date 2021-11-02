1f51f68a1aabe151b1258107e631bd2e7b0ea20f
NicolasJbo <57155671+NicolasJbo@users.noreply.github.com>
2021-11-02 20:48:34 -0300

    Version 2

--- OperativaII-Angular/src/app/components/home/home.component.ts
+++ OperativaII-Angular/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from 'src/app/models/city';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  citiesList :Array<City>=[{cityId: '0',name: 'Mar del Plata'},
+  {cityId: '1',name: 'Carlos Paz'},{cityId: '2',name: 'Bariloche'},{cityId: '2',name: 'Carlos'},
+  {cityId: '4',name: 'Usuahia'},{cityId: '5',name: 'Salta'},{cityId: '6',name: 'Jujuy'},
+  {cityId: '7',name: 'Neuquen'},{cityId: '8',name: 'San Luis'},{cityId: '9',name: 'Rosario'},{cityId: '10',name: 'San Juan'}];
 
   ngOnInit(): void {
+  }
+  listAdd : Array<City>= [];
+
+  addCity(c :City){
+    
+    let contains = false;
+    let cityID = c.cityId;
+
+    this.listAdd.forEach(function (c) {
+      if(cityID == c.cityId) contains = true;
+    })
+
+    if (contains)
+      console.log("La ciudad ya existe");
+    else {
+      this.listAdd.push(c);
+    }
+  }
+  removeCity(city:City){
+    this.listAdd.forEach((c, index) => {
+      if (c.cityId === city.cityId)
+        this.listAdd.splice(index, 1);
+    });
   }
 
 }
