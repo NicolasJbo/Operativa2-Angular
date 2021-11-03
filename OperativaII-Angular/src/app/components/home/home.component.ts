7a980088c17715af177c2400ebe61d79621547cf
NicolasJbo <57155671+NicolasJbo@users.noreply.github.com>
2021-11-03 18:40:59 -0300

    Ocultar botones

--- OperativaII-Angular/src/app/components/home/home.component.ts
+++ OperativaII-Angular/src/app/components/home/home.component.ts
@@ -9,15 +9,15 @@ import { City } from 'src/app/models/city';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-  citiesList :Array<City>=[{cityId: '0',name: 'Mar del Plata'},
-  {cityId: '1',name: 'Carlos Paz'},{cityId: '2',name: 'Bariloche'},{cityId: '2',name: 'Carlos'},
-  {cityId: '4',name: 'Usuahia'},{cityId: '5',name: 'Salta'},{cityId: '6',name: 'Jujuy'},
-  {cityId: '7',name: 'Neuquen'},{cityId: '8',name: 'San Luis'},{cityId: '9',name: 'Rosario'},{cityId: '10',name: 'San Juan'}];
+  citiesList :Array<City>=[{cityId: '0',name: 'Mar del Plata',show :false },
+  {cityId: '1',name: 'Carlos Paz',show :false},{cityId: '2',name: 'Bariloche',show :false},{cityId: '2',name: 'Esta ciudad esta mal! (noc cual es)',show :false},
+  {cityId: '4',name: 'Usuahia',show :false},{cityId: '5',name: 'Salta',show :false},{cityId: '6',name: 'Jujuy',show :false},
+  {cityId: '7',name: 'Neuquen',show :false},{cityId: '8',name: 'San Luis',show :false},{cityId: '9',name: 'Rosario',show :false},{cityId: '10',name: 'San Juan',show :false}];
 
   ngOnInit(): void {
   }
   listAdd : Array<City>= [];
-
+  
   addCity(c :City){
     
     let contains = false;
@@ -31,12 +31,15 @@ export class HomeComponent implements OnInit {
       console.log("La ciudad ya existe");
     else {
       this.listAdd.push(c);
+      console.log( this.listAdd);
+      c.show=true;
     }
   }
   removeCity(city:City){
     this.listAdd.forEach((c, index) => {
       if (c.cityId === city.cityId)
         this.listAdd.splice(index, 1);
+        c.show=false;
     });
   }
 
--- OperativaII-Angular/src/app/models/city.ts
+++ OperativaII-Angular/src/app/models/city.ts
@@ -1,4 +1,5 @@
 export class City {
     cityId!: string;
     name!: string;  
+    show !: boolean;
 }
